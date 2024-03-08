7d76f590d22c7168bd8ea8fdd307dcb18011a69c
Felix <femarleycode@gmail.com>
2024-03-08 20:12:35 +0000

    working data read

--- test2.py
+++ test2.py
@@ -1,4 +1,23 @@
 import pandas as pd
+import pyreadstat
 
-df = pd.read_spss('data/BrexitAttitudes_Data.sav')
+# Read the SPSS file
+df, meta = pyreadstat.read_sav('data/BrexitAttitudes_Data.sav')
 
+print("start")
+
+# Print variable names
+print(meta.column_names)
+
+# Print variable labels
+print(meta.column_labels)
+
+# Print value labels (if any)
+print(meta.variable_value_labels)
+
+print("stop")
+
+# # Print variable types (numeric or string)
+# print(meta.column_types)
+
+# # Now you can work with the DataFrame 'df'
