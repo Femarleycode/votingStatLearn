023aaa1dc67dd3e4e2a548cf87ea24f8f8184085
Felix <femarleycode@gmail.com>
2024-03-08 20:36:56 +0000

    added shape

--- test2.py
+++ test2.py
@@ -15,9 +15,17 @@ print(meta.column_labels)
 # Print value labels (if any)
 print(meta.variable_value_labels)
 
+# Get the number of rows and columns
+num_rows, num_cols = df.shape
+
+# Print the number of rows and columns
+print("Number of rows:", num_rows)
+print("Number of columns:", num_cols)
+
 print("stop")
 
 # # Print variable types (numeric or string)
 # print(meta.column_types)
 
 # # Now you can work with the DataFrame 'df'
+
