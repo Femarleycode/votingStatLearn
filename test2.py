d66414c78978c0e14fb53e6137adcd13865d6910
Felix <femarleycode@gmail.com>
2024-03-10 22:58:22 +0000

    correlation matrix

--- test2.py
+++ test2.py
@@ -19,7 +19,7 @@ print("start")
 
 
 # Print top 5 rows
-print(df.head())
+# print(df.head())
 
 # Histograms
 # df.hist(figsize=(15,15))
@@ -28,43 +28,52 @@ print(df.head())
 
 
 
-plt.figure(figsize=(14,6))
-plt.subplot(1,2,1)
-df['EURef2016'].value_counts().plot(kind='bar')
-plt.title("Bar")
+# plt.figure(figsize=(14,6))
+# plt.subplot(1,2,1)
+# df['EURef2016'].value_counts().plot(kind='bar')
+# plt.title("Bar")
 
-plt.subplot(1,2,2)  
-df['EURef2016'].value_counts().plot(kind='pie',labels=["Yes", "No", "Skip", "Forgot"], autopct="%.2f%%")
-plt.title("Pie")
-plt.show()
+# plt.subplot(1,2,2)  
+# df['EURef2016'].value_counts().plot(kind='pie',labels=["Yes", "No", "Skip", "Forgot"], autopct="%.2f%%")
+# plt.title("Pie")
+# plt.show()
 
 
 # Check for age distribution for candidates backing out after Round 1 and 2
 # 0 stands for Retained , 1 stands for Left
 
-# Drop people who forgot or didn't vote
-print(df['EURef2016'].value_counts())
-# Assuming df is your DataFrame and values_to_drop is a list of values
-values_to_drop = [3.0, 4.0]  # List of values to drop
+# # Drop people who forgot or didn't vote
+# print(df['EURef2016'].value_counts())
+# # Assuming df is your DataFrame and values_to_drop is a list of values
+# values_to_drop = [3.0, 4.0]  # List of values to drop
 
-# Drop rows where column 'age' contains any value from the list values_to_drop
-df = df[~df['EURef2016'].isin(values_to_drop)]
-print(df['EURef2016'].value_counts())
-
-
+# # Drop rows where column 'age' contains any value from the list values_to_drop
+# df = df[~df['EURef2016'].isin(values_to_drop)]
+# print(df['EURef2016'].value_counts())
 
 
 
-# Plot
-plt.figure(figsize=(10, 6))
-sns.countplot(data=df, x='age', hue='EURef2016')
-plt.title('Distribution of EU Referendum Votes by Age')
-plt.xlabel('Age')
-plt.ylabel('Count')
-plt.xticks(rotation=45)
-plt.legend(title='EU Referendum Vote')
-plt.tight_layout()
+# # Plot
+# plt.figure(figsize=(10, 6))
+# sns.countplot(data=df, x='age', hue='EURef2016')
+# plt.title('Distribution of EU Referendum Votes by Age')
+# plt.xlabel('Age')
+# plt.ylabel('Count')
+# plt.xticks(rotation=45)
+# plt.legend(title='EU Referendum Vote')
+# plt.tight_layout()
+# plt.show()
+
+
+# Correlation Matrix
+## Choose numerical or ordinal columns for correlation matrix
+non_cat = df[['age', 'political_attention', 'newspaper_readership', 'profile_gross_personal', 
+              'profile_gross_household', 'education_age', 'socialgradeCIE1', 'socialgradeCIE2', 
+              'Weight', 'Hindsight', 'Poscountry', 'Posind', 'Govthand', 'Tradeimmig', 
+              'BrexitID', 'Leaverimport', 'Remainimport', 'Genecon']]
+
+## Create correlation matrix
+corr_matrix=non_cat.corr()
+fig,ax=plt.subplots(figsize=(15,10))
+sns.heatmap(corr_matrix,annot=True)
 plt.show()
-
-
-
